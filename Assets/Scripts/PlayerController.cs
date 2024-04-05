48038c5d29e0a4d905bf27c45f472a6fbf0cd4cd
JP_Gazmuri <jpgazmuri@miuandes.cl>
2024-04-05 16:57:51 -0300

    Running button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    private bool IsRunning = false;
 
     void Update()
     {
@@ -11,6 +12,19 @@ public class PlayerController : MonoBehaviour
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         GetComponent<Rigidbody2D>().velocity = movement * speed;
+        if (!IsRunning && Input.GetKey(KeyCode.Space))
+        {
+            IsRunning = true;
+            speed = 15f;
+        }
+        else if (IsRunning && !Input.GetKey(KeyCode.Space))
+        {
+
+             IsRunning = false;
+             speed = 5f;
+        
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
