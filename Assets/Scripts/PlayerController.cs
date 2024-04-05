36c9ecb39a452513bd4ae23ecb2a4c14a443b921
joaquinvaras <jevaras@miuandes.cl>
2024-04-05 16:59:40 -0300

    move animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,25 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private bool isMoving;
     private Vector2 input;
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         if (isMoving!=true)
         {
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
+            
+            Debug.Log("This is input.x" + input.x);
+            Debug.Log("This is input.y" + input.y);
+
+            animator.SetFloat("MoveX", input.x);
+            animator.SetFloat("MoveY", input.y);
+
             if (input != Vector2.zero)
             {
                 var targetPos = transform.position;
@@ -22,6 +35,8 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(Move(targetPos));
             }
         }
+
+        animator.SetBool("isMoving", isMoving);
     }
 
 
