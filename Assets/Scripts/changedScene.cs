1f0f994d728b40fce327e0d630c81fd725bb2a70
benjamin gonzalez <bigonzalez@miuandes.cl>
2024-04-05 18:20:09 -0300

    feature: add second scene
    se añadio ademas el cambio de escena

---
+++ Assets/Scripts/changedScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changedScene : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Verificar si el objeto que entró en el trigger tiene el tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Cambiar a la siguiente escena
+            Debug.Log("CAMBIO DE ESCENA");
+            SceneManager.LoadScene("piso1");
+        }
+    }
+}
