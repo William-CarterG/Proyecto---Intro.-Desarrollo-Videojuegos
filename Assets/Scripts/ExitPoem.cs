708093d682208bdbba2282fc5e424e1558a1e0e9
Víctor Rivas C. <vhrivas@miuandes.cl>
2024-05-25 18:15:37 -0400

    acertijoPoema

---
+++ Assets/Scripts/ExitPoem.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;  // El nombre de la escena a la que quieres cambiar
+
+    void Update()
+    {
+        // Verifica si se presionó la tecla Escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Carga la escena especificada
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
